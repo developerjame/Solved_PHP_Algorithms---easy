40b304a72947c3bec3ca36c11ae93a423d2caca8
developerjame <119099522+developerjame@users.noreply.github.com>
2024-03-06 23:05:26 +0300

    question

---
+++ Sqrt(x)/solution.php
@@ -0,0 +1,33 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer $x
+     * @return Integer
+     */
+    function mySqrt($x) {
+       if($x==0 || $x==1) return $x;
+       
+       $left = 1;
+       $right = $x;
+       $ans = 0;
+
+       while($left<=$right){
+        $mid = $left + floor(($right-$left)/2);
+        if($mid <= floor($x / $mid)){
+            $ans = $mid;
+            $left = $mid + 1;
+        }else{
+            $right = $mid - 1;
+        }
+       }
+
+       return $ans;
+    }
+}
+
+$x = 25;
+$solution = new Solution();
+$result = $solution->mySqrt($x);
+echo "The squareroot of $x is: $result";
+?>
