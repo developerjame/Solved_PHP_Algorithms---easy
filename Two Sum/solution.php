d10e71950a80687d5dd87f73120813cecf534b09
developerjame <119099522+developerjame@users.noreply.github.com>
2024-03-08 15:39:18 +0300

    solution

--- Two Sum/solution.php
+++ Two Sum/solution.php
@@ -3,13 +3,20 @@ class Solution{
     public function twoSum($nums, $target){
         $map = array();
 
+        // Iterate through the array
         for($i=0;$i<count($nums);$i++){
+            // Calculate the complement for the current number
             $complement = $target-$nums[$i];
+
+            // Check if the complement exists in the hashmap
             if(array_key_exists($complement, $map)){
+                // If found, return the indices
                 return [$map[$complement], $i];
             }
+            // Otherwise, store the current number and its index in the hashmap
             $map[$nums[$i]] = $i;
         }
+        // Return an empty array if no solution is found
         return [];
     }
 }
