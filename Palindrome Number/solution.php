05489d68bc53e4ef5ec89c6f88cf1bfcaeb32cd4
developerjame <119099522+developerjame@users.noreply.github.com>
2024-03-11 12:07:24 +0300

    solution

--- Palindrome Number/solution.php
+++ Palindrome Number/solution.php
@@ -6,7 +6,19 @@ class Solution {
      * @return Boolean
      */
     function isPalindrome($x) {
-        
+        $str = (string) $x;
+        $length = strlen($str);
+
+        for($i=0;$i<$length/2;$i++){
+            if($str[$i] !== $str[$length-$i-1]){
+                return false;
+            }
+        }
+        return true;
     }
 }
+$x = 241;
+$solution = new Solution();
+$result = $solution->isPalindrome($x);
+echo $result ? "True":"False";
 ?>
