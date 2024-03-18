f473d5aca03fd3118829a1549626d9715657fe7f
developerjame <119099522+developerjame@users.noreply.github.com>
2024-03-18 16:38:35 +0300

    solution

---
+++ Longest Common Prefix/solution.php
@@ -0,0 +1,38 @@
+<?php
+class Solution {
+
+    /**
+     * @param String[] $strs
+     * @return String
+     */
+    function longestCommonPrefix($strs) {
+        if (empty($strs)) return "";
+
+        $prefix = $strs[0]; // Start with the first string as the initial prefix
+    
+        // Iterate through the rest of the strings
+        for ($i = 1; $i < count($strs); $i++) {
+            $length = min(strlen($prefix), strlen($strs[$i]));
+            $temp = "";
+    
+            // Compare characters of the current string with the current prefix
+            for ($j = 0; $j < $length; $j++) {
+                if ($prefix[$j] === $strs[$i][$j]) {
+                    $temp .= $prefix[$j]; // Append matching characters to $temp
+                } else {
+                    break; // Stop as soon as a mismatch is found
+                }
+            }
+    
+            $prefix = $temp; // Update prefix to contain the common characters
+            if ($prefix === "") break; // If prefix becomes empty, no need to continue
+        }
+    
+        return $prefix;
+    }
+}
+$solution = new Solution();
+
+$strs = ["Flow", "float", "flight"];
+echo $solution->longestCommonPrefix($strs) . "\n";
+?>
