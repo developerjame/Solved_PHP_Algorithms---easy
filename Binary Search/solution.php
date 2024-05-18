2e6ef2148e309b4ea0645ad18ca6b388ffd4d857
developerjame <119099522+developerjame@users.noreply.github.com>
2024-05-18 11:23:59 +0300

    solution

--- Binary Search/solution.php
+++ Binary Search/solution.php
@@ -1,5 +1,19 @@
 <?php
 function binarysearch($arr, $target){
-    
+    $left = 0;
+    $right = count($arr)-1;
+
+    while($left<=$right){
+        $mid = floor(($left+$right)/2);
+        if($arr[$mid] == $target){
+            return $mid;
+        }
+        if($arr[$mid] > $target){
+            $right = $mid-1;
+        }else{
+            $left = $mid+1;
+        }
+    }
+    return -1;
 }
 ?>
