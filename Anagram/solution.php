bd31d098ea5a93beec3b8611f4d5c46aac913c33
developerjame <119099522+developerjame@users.noreply.github.com>
2024-05-19 19:05:39 +0300

    solution

--- Anagram/solution.php
+++ Anagram/solution.php
@@ -3,4 +3,6 @@ function isAnagram($str1, $str2){
     $newstr1 = strtolower(str_replace(' ','',$str1));
     $newstr2 = strtolower(str_replace(' ','',$str2));
 }
+$string1 = "listen";
+$string2 = "silent";
 ?>
