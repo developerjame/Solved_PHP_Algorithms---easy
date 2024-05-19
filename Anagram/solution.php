87e2da2b95f905c7901bfe6b59f3957f0a704bb0
developerjame <119099522+developerjame@users.noreply.github.com>
2024-05-19 22:10:49 +0300

    solution

--- Anagram/solution.php
+++ Anagram/solution.php
@@ -2,7 +2,22 @@
 function isAnagram($str1, $str2){
     $newstr1 = strtolower(str_replace(' ','',$str1));
     $newstr2 = strtolower(str_replace(' ','',$str2));
+    if(strlen($newstr1) != strlen($newstr2)){
+        return false;
+    }
+    $sortedStr1 = str_split($newstr1);
+    $sortedStr2 = str_split($newstr2);
+
+    sort($sortedStr1);
+    sort($sortedStr2);
+
+    if($sortedStr1 === $sortedStr2){
+        return true;
+    }else{
+        return false;
+    }
 }
 $string1 = "listen";
 $string2 = "silent";
+echo isAnagram($string1, $string2) ? "They are anagrams":"They are not anagrams";
 ?>
